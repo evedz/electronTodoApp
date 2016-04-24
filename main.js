523fb7e37b4266ee2d266e37ea3267c624ffc735
Maxim Kochetov <maxkochetov@gmail.com>
2016-04-24 22:19:10 +0300

    Refactoring

--- main.js
+++ main.js
@@ -1,86 +1,101 @@
 var app = angular.module('todoApp', ['ngMaterial']);
 
-app.controller('AppCtrl', ['$scope', '$mdDialog', '$mdToast', function($scope, $mdDialog, $mdToast){
+app.run(function () {
+    var existingTasks = localStorage.getItem("db");
 
-
-    // Adding an item to localStorage in case it is empty.
-    if (!localStorage.getItem("taskName")) {
-        localStorage.setItem("taskName", JSON.stringify({
-            1452435614966: {
-                name: "Hello",
-                done: false,
-                id: 1452435614966
-            }
-        }))
+    if (!existingTasks || !angular.isArray(JSON.parse(existingTasks))) {
+        localStorage.setItem("db", JSON.stringify([]));
     }
+});
 
-    $scope.options = {
+app.factory('tasksFactory', function () {
+    return {
+        addTask: function (task) {
+            var existingTasks = this.getTasks();
+
+            var newTaskDate = new Date().getTime();
+
+            var obj = {};
+
+            obj[newTaskDate] = {
+                'name': task,
+                'done': false,
+                'id': newTaskDate
+            };
+
+            existingTasks.push(obj);
+
+            localStorage.setItem("db", JSON.stringify(existingTasks));
+        },
+        getTasks: function () {
+            return JSON.parse(localStorage.getItem("db"));
+        },
+        syncScopeWithStorage: function (updated) {
+            localStorage.setItem("db", JSON.stringify(updated));
+        }
+    }
+});
+
+app.controller('AppCtrl', function ($scope, $mdDialog, $mdToast, tasksFactory) {
+
+    $scope.userTypedText = '';
+
+    $scope.meta = {
         name: 'My Todo List',
         show: 'All tasks'
     };
 
-    $scope.navigationBarItems = [
+    $scope.categoriesList = [
         {'title': 'All tasks', 'icon': 'list'},
         {'title': 'Todo', 'icon': 'assignment'},
         {'title': 'Done', 'icon': 'done'}
     ];
 
-    $scope.locStorTasks = JSON.parse(localStorage.getItem("taskName"));
+    $scope.DB = tasksFactory.getTasks();
 
-    $scope.objectDone = function () {
-        var t = $scope.locStorTasks;
-        localStorage.setItem("taskName", JSON.stringify(t));
+    $scope.addNewTask = function (task) {
+        tasksFactory.addTask(task);
+        $scope.DB = tasksFactory.getTasks();
+        $scope.userTypedText = '';
+        $mdToast.showSimple(task + " was added!");
     };
 
-    $scope.objectDelete = function (item) {
-        var t = $scope.locStorTasks;
-        delete t[item];
-        localStorage.setItem("taskName", JSON.stringify(t));
+    $scope.markTaskAsDone = function () {
+        tasksFactory.syncScopeWithStorage($scope.DB);
     };
 
-    $scope.setContent = function (content) {
-        $scope.options.show = content;
+    $scope.objectDelete = function (id) {
+        $scope.DB.splice(id, 1);
+        localStorage.setItem("db", JSON.stringify($scope.DB));
+        $scope.DB = tasksFactory.getTasks();
     };
 
-    $scope.addToLocalStorage = function (name) {
-
-        var existingTasks = JSON.parse(localStorage.getItem("taskName"));
-
-        var newTaskDate = new Date().getTime();
-
-        existingTasks[newTaskDate] = {
-            'name': name,
-            'done': false,
-            'id': newTaskDate
-        };
-
-        localStorage.setItem("taskName", JSON.stringify(existingTasks));
-
-        setTimeout(function () {location.reload();}, 150);
+    $scope.changeTab = function (content) {
+        $scope.meta.show = content;
     };
 
-    $scope.showAdd = function(ev) {
+    $scope.showAdd = function (ev) {
         $mdDialog.show({
             controller: AddDialogCtrl,
             template: '<md-dialog aria-label="User Form"> <md-content class="md-padding"> <form name="userForm"> <md-input-container> <label>New Task</label> <input ng-model="newTask" placeholder="Placeholder text"> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="cancel()"> Cancel </md-button> <md-button ng-click="add(newTask)" class="md-primary"> Add task </md-button> </div></md-dialog>',
             targetEvent: ev
         })
-            .then(function(task) {
-                $scope.addToLocalStorage(task);
-                $mdToast.showSimple('Task "' + task + '" was added!');
-            });
+        .then(function (task) {
+            $scope.addNewTask(task);
+            $mdToast.showSimple('Task "' + task + '" was added!');
+        });
     };
 
-}]);
+});
 
 function AddDialogCtrl($scope, $mdDialog) {
-    $scope.hide = function() {
+    $scope.hide = function () {
         $mdDialog.hide();
     };
-    $scope.cancel = function() {
+    $scope.cancel = function () {
         $mdDialog.cancel();
     };
-    $scope.add = function(task) {
+    $scope.add = function (task) {
         $mdDialog.hide(task);
     };
 }
